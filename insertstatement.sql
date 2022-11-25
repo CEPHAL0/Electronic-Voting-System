71eac87d0d1283ff233506ca3111ebe3b084d066
Sharad Sharma <xaradxarma@gmail.com>
2022-11-25 20:31:28 +0545

    Insert Statements for reference

---
+++ insertstatement.sql
@@ -0,0 +1,42 @@
+-- voterid(3DIGIT),name,age,gender(MALE/FEMALE),area(Capitalized),votestatus(TRUE/FALSE)
+#Voter_Table()
+insert into voter values("173","FAYDRA","24","FEMALE","KATHMANDU",TRUE);
+insert into voter values("771","FAIR","28","MALE","BIRATNAGAR",FALSE);
+insert into voter values("223","DEMETRI","30","MALE","CHITWAN",TRUE);
+insert into voter values("102","VLADIMIR","18","MALE","GORKHA",TRUE);
+insert into voter values("761","TRACIE","44","FEMALE","KATHMANDU",FALSE);
+insert into voter values("461","SANDRA","67","FEMALE","KATHMANDU",FALSE);
+insert into voter values("523","RENE","91","FEMALE","GORKHA",TRUE);
+insert into voter values("189","LYON","55","MALE","CHITWAN",TRUE);
+insert into voter values("172","ANDRE","54","MALE","BIRATNAGAR",TRUE);
+insert into voter values("101","FAITH","73","FEMALE","BIRATNAGAR",FALSE);
+insert into voter values("100","JASON","44","MALE","GORKHA",FALSE);
+insert into voter values("801","JACOB","24","MALE","CHITWAN",FALSE);
+insert into voter values("888","JOHN","21","MALE","GORKHA",TRUE);
+insert into voter values("825","SAUL","20","MALE","CHITWAN",FALSE);
+insert into voter values("873","MIKE","56","MALE","BIRATNAGAR",FALSE);
+insert into voter values("837","SANDY","77","FEMALE","GORKHA",FALSE);
+insert into voter values("120","MICHELLE","70","FEMALE","KATHMANDU",TRUE);
+insert into voter values("165","JOANA","84","FEMALE","CHITWAN",FALSE);
+insert into voter values("177","AMANDA","44","FEMALE","CHITWAN",FALSE);
+insert into voter values("206","AUSTIN","47","MALE","BIRATNAGAR",TRUE);
+insert into voter values("699","BROOKLYN","46","MALE","KATHMANDU",FALSE);
+insert into voter values("420","GUSTAVO","34","MALE","KATHMANDU",TRUE);
+insert into voter values("690","BARACK","22","MALE","BIRATNAGAR",TRUE);
+insert into voter values("848","HENRY","20","MALE","CHITWAN",TRUE);
+insert into voter values("777","HANK","19","MALE","BIRATNAGAR",FALSE);
+
+#Candidate_Tabel
+-- candidate_id(4didgit),fname(capital),lname(capital),area(capital),votereceived(int more than 1-100)
+insert into Candidate values("1029","EDWARD","SMITH","BIRATNAGAR",55);
+insert into Candidate values("6113","DUSTY","TANT","CHITWAN",55);
+insert into Candidate values("2920","MICHEAL","OXLONG","KATHMANDU",55);
+insert into Candidate values("4233","KRISTIE","WINDUS","GORKHA",55);
+insert into Candidate values("7647","ALCIE","MARCHAM","KATHMANDU",55);
+insert into Candidate values("5492","BORAT","SAGDIEV","KATHMANDU",55);
+insert into Candidate values("6942","AZAMAT","BAGATOV","CHITWAN",55);
+insert into Candidate values("8911","ANTHONY","JOHNSON","CHITWAN",55);
+insert into Candidate values("2382","PETA","AIRES","BIRATNAGAR",55);
+insert into Candidate values("3143","KAHLIL","BLASCHKE","GORKHA",55);
+insert into Candidate values("1299","BRUCE","BUUFFER","BIRATNAGAR",55);
+insert into Candidate values("1329","MORTIE","FASHION","BIRATNAGAR",55);
